8d95762def1aa3350d991ade222bc28f809cd8a8
oyewole timilehin <oyewoleoluwatimilehin2@gmail.com>
2024-11-16 09:54:34 -1100

    UPDATE

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ServiceData from "../pages/ServicesData";
 
 const Service = () => {
-  console.log(ServiceData);
   return (
     <div className=" max-w-[1640px]  mx-auto px-4 py-16 ">
       {/* SERVICE CONTENT */}
@@ -12,18 +11,29 @@ const Service = () => {
           <h2 className=" text-2xl text-black font-bold">WHAT WE DO</h2>
         </div>
         {/*  */}
-        <div>
+        <div className="flex flex-wrap justify-center items-center  gap-4">
           {ServiceData.map(({ id, img: Icon, name, description }) => {
             return (
-              <div key={id}>
-                <div>{Icon && <Icon />} </div>
-                <div>
+              <div
+                className=" flex flex-col items-start w-[300px] h-auto"
+                key={id}
+              >
+                <div className="rounded-lg shadow-lg py-8 px-8 w-[full] h-auto ">
+                  {Icon && (
+                    <Icon className=" text-black w-[250px] h-[250px]  md:w-[170px] md:h-[170px] lg:w-[170px] lg:h-[170px] " />
+                  )}
+                </div>
+                <div className=" py-4 w-full h-auto ">
                   <h1>{name} </h1>
                   <p>{description} </p>
                 </div>
               </div>
             );
           })}
+          {/* BUTTON */}
+          <div>
+            <button>SEND PACKAGE</button>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/ServicesData.js
+++ src/pages/ServicesData.js
@@ -1,19 +1,23 @@
-import { CiDeliveryTruck } from "react-icons/ci";
+import { TbTruckDelivery } from "react-icons/tb";
+import { LuPackageOpen } from "react-icons/lu";
+import { MdOutlineFoodBank } from "react-icons/md";
 
 const ServiceData = [
   {
     id: 1,
-    img: CiDeliveryTruck, // Component reference
+    img: TbTruckDelivery,
     name: "SEND PACKAGE",
     description: "Ship Bulk and Single Items Locally and Internationally.",
   },
   {
     id: 2,
+    img: LuPackageOpen,
     name: "TRACK PACKAGE",
     description: "Ship Bulk and Single Items Locally and Internationally.",
   },
   {
     id: 3,
+    img: MdOutlineFoodBank,
     name: "BUY FOOD",
     description: "Order Food from your Favorite Restaurants.",
   },
